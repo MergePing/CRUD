161b9c0978cee3db1dee395d936ee66a20927bfe
Pear1yCode <gh_wns@naver.com>
2024-10-10 17:46:33 +0900

    CRUD

--- src/main/java/com/ohgiraffers/model/dao/EmployeeMapper_LHJ.java
+++ src/main/java/com/ohgiraffers/model/dao/EmployeeMapper_LHJ.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.model.dao;
 
+import com.ohgiraffers.model.dto.EmployeeDTO;
+
+import java.util.List;
+
 public interface EmployeeMapper_LHJ {
+
+    static List<EmployeeDTO> allEmployee() {
+        return List.of();
+    }
 }
--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -1,4 +1,28 @@
 package com.ohgiraffers.run;
 
+import com.ohgiraffers.controller.EmployeeController_LHJ;
+
+import java.util.Scanner;
+
 public class Application {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        EmployeeController_LHJ empCon = new EmployeeController_LHJ();
+
+        do {
+            System.out.println("========== 이호준 : 마이바티스 기본 구현 실습 ==========");
+            System.out.println("1. 전체 조회");
+            int numAnswer = sc.nextInt();
+
+            switch (numAnswer) {
+                case 1 : empCon.allEmployee();
+            }
+        }while(true);
+    }
+
+
+
+
+
 }
