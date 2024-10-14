61b988efc937e1bacea472add9d0f9b6efbf07c7
manbeao <142187096+manbeao@users.noreply.github.com>
2024-10-14 14:51:12 +0900

    Update PrintResult_LES.java

--- src/main/java/com/ohgiraffers/controller/PrintResult_LES.java
+++ src/main/java/com/ohgiraffers/controller/PrintResult_LES.java
@@ -10,14 +10,14 @@ public class PrintResult_LES {
             System.out.println(emp);
         }
         System.out.println();
-        System.out.println("전체조회를 완료했습니다");
+        System.out.println("전체조회를 완료했습니다");break;
     }
 
     public void printerror(String error) {
         String errorMessage = "";
         switch (error){
-            case "selectAllEmp" : errorMessage ="전제 조회에 실패했습니다";
-            case "insertEmpInfo" : errorMessage ="정보 등록에 실패했습니다";
+            case "selectAllEmp" : errorMessage ="전제 조회에 실패했습니다"; break;
+            case "insertEmpInfo" : errorMessage ="정보 등록에 실패했습니다";break;
         }
         System.out.println();
         System.out.println(errorMessage);
@@ -27,7 +27,7 @@ public class PrintResult_LES {
     public void printSuccessMessage(String successCode) {
         String successMessage ="";
         switch (successCode){
-            case "insertEmpInfo" : successMessage = "직원 정보 추가에 성공했습니다";
+            case "insertEmpInfo" : successMessage = "직원 정보 추가에 성공했습니다";break;
         }
         System.out.println();
         System.out.println(successMessage);
