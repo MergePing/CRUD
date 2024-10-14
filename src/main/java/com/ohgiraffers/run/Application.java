44653678ae2eb3e0ff74217cae31e3d555633eea
Pear1yCode <gh_wns@naver.com>
2024-10-14 16:54:44 +0900

    Application

--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -11,8 +11,8 @@ public class Application {
         View_LHJ vLHJ = new View_LHJ();
 
         while (true) {
-            System.out.println("확인할 수강생의 이름을 선택하세요.");
-            System.out.println("1. 황순빈, 2. 조영석, 3. 이은서, 4. 이호준");
+            System.out.println("직원조회를 만든 수강생의 이름을 선택하세요.");
+            System.out.println("1. 황순빈, 2. 조영석, 3. 이은서, 4. 이호준, 5. 프로그램 종료");
             int answer = sc.nextInt();
             switch (answer) {
                 case 1 : vHSB.View_HSB(); break;
