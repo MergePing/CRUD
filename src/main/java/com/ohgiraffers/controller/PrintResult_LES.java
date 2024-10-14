ab31a2ef7d7965ca63c2c5b29ce7e2a978d7226e
manbeao <142187096+manbeao@users.noreply.github.com>
2024-10-14 14:53:44 +0900

    Update PrintResult_LES.java

--- src/main/java/com/ohgiraffers/controller/PrintResult_LES.java
+++ src/main/java/com/ohgiraffers/controller/PrintResult_LES.java
@@ -16,10 +16,10 @@ public class PrintResult_LES {
     public void printerror(String error) {
         String errorMessage = "";
         switch (error){
-            case "selectAllEmp" : errorMessage ="직원 전제 조회에 실패했습니다";
-            case "insertEmpInfo" : errorMessage ="직원 정보 등록에 실패했습니다";
-            case "changeEmpInfo" : errorMessage = "직원 정보 수정에 실패했습니다";
-            case "deleteEmpInfo" : errorMessage = "직원 정보 삭제에 실패했습니다";
+            case "selectAllEmp" : errorMessage ="직원 전제 조회에 실패했습니다";break;
+            case "insertEmpInfo" : errorMessage ="직원 정보 등록에 실패했습니다";break;
+            case "changeEmpInfo" : errorMessage = "직원 정보 수정에 실패했습니다";break;
+            case "deleteEmpInfo" : errorMessage = "직원 정보 삭제에 실패했습니다";break;
         }
         System.out.println();
         System.out.println(errorMessage);
@@ -29,9 +29,9 @@ public class PrintResult_LES {
     public void printSuccessMessage(String successCode) {
         String successMessage ="";
         switch (successCode){
-            case "insertEmpInfo" : successMessage = "직원 정보 추가에 성공했습니다";
-            case "changeEmpInfo" : successMessage = "직원 정보 수정에 성공했습니다";
-            case "deleteEmpInfo" : successMessage = "직원 정보 삭제에 성공했습니다";
+            case "insertEmpInfo" : successMessage = "직원 정보 추가에 성공했습니다";break;
+            case "changeEmpInfo" : successMessage = "직원 정보 수정에 성공했습니다";break;
+            case "deleteEmpInfo" : successMessage = "직원 정보 삭제에 성공했습니다";break;
         }
         System.out.println();
         System.out.println(successMessage);
